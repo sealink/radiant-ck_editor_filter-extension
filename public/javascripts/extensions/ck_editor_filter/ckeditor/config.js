478f80850b56c2878988e038affcfbc0a050ea39
Jack McNicol <jack.mcnicol@sealink.com.au>
2013-06-27 15:00:57 +0930

    updated configs

--- public/javascripts/extensions/ck_editor_filter/ckeditor/config.js
+++ public/javascripts/extensions/ck_editor_filter/ckeditor/config.js
@@ -1,10 +1,48 @@
-﻿/*
-Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
-For licensing, see LICENSE.html or http://ckeditor.com/license
-*/
+/**
+ * @license Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.html or http://ckeditor.com/license
+ */
 
-CKEDITOR.editorConfig = function( config )
-{
+CKEDITOR.editorConfig = function( config ) {
+
+  config.startupOutlineBlocks = true;
+  config.autoParagraph = false;
+  config.skin = "moono";
+  config.extraPlugins = 'codemirror,scayt,autogrow,MediaEmbed';
+  config.colorButton_enableMore = false;
+  // Code Cleanup
+  config.protectedSource.push(/<r:([\S]+)*>.*<\/r:\1>/g);
+  config.protectedSource.push(/<r:[^>\/]*\/>/g);
+  config.emailProtection = "encode";
+  config.forcePasteAsPlainText = true;
+  config.pasteFromWordPromptCleanup = true;
+  config.pasteFromWordNumberedHeadingToList = true;
+  // Auto Spell correct
+  config.scayt_autoStartup = true;
+  // Autogrow settings
+  config.height = 300;
+  config.autoGrow_onStartup = true;
+  config.autoGrow_maxHeight = 800;
+  config.autoGrow_minHeight = 300;
+  config.autoGrow_bottomSpace = 20;
+  // For allowing classes on divs - http://docs.ckeditor.com/#!/api/CKEDITOR.config
+  config.allowedContent = true;
+  // Nice source highlighting
+  config.codemirror = {
+    highlightActiveLine: false,
+    theme: 'rubyblue'
+  };
+  // Your custom stylesheet
+  // config.contentsCss = '/stylesheets/bootstrap.css';
+  config.toolbar = [
+    ['Format'],
+    ['Bold','Italic','Strike','-','Subscript','Superscript'],
+    ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
+    ['Undo','Redo','-','Find','Replace','-', 'Paste', 'RemoveFormat', 'Scayt'],
+    ['Link','Unlink','Anchor'],
+    ['Image', 'MediaEmbed', 'Table','HorizontalRule','SpecialChar','-','ShowBlocks'],
+    ['Source']
+  ];
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
--- public/javascripts/extensions/ck_editor_filter/radiant.ckeditor.js
+++ public/javascripts/extensions/ck_editor_filter/radiant.ckeditor.js
@@ -1,95 +1,108 @@
 function instantiateCkEditor(partIndex){
-	CKEDITOR.config.startupOutlineBlocks = true
-	CKEDITOR.config.colorButton_enableMore = false
-	CKEDITOR.config.protectedSource.push( /<r:([\S]+)*>.*<\/r:\1>/g )
-	CKEDITOR.config.protectedSource.push( /<r:[^>\/]*\/>/g )
-	CKEDITOR.config.extraPlugins = 'paperclipped,MediaEmbed,radiantpreview'
-	CKEDITOR.config.forcePasteAsPlainText = true
-	CKEDITOR.config.height = 500
-	CKEDITOR.config.toolbar =
-	[
-		['Styles','Format'],
+  // Fixes parts with underscores or spaces in the name
+  partIndex = partIndex.replace(/(_| )/g,'-');
+
+  CKEDITOR.config.extraPlugins = 'paperclipped,autogrow,radiantpreview,codemirror,scayt,MediaEmbed';
+
+  CKEDITOR.config.startupOutlineBlocks = true;
+  CKEDITOR.config.autoParagraph = false;
+  CKEDITOR.config.skin = "moono";
+  CKEDITOR.config.colorButton_enableMore = false;
+  // Code Cleanup
+  CKEDITOR.config.protectedSource.push(/<r:([\S]+)*>.*<\/r:\1>/g);
+  CKEDITOR.config.protectedSource.push(/<r:[^>\/]*\/>/g);
+  CKEDITOR.config.emailProtection = "encode";
+  CKEDITOR.config.forcePasteAsPlainText = true;
+  CKEDITOR.config.pasteFromWordPromptCleanup = true;
+  CKEDITOR.config.pasteFromWordNumberedHeadingToList = true;
+  // Auto Spell correct
+  CKEDITOR.config.scayt_autoStartup = true;
+  // Autogrow settings
+  CKEDITOR.config.height = 300;
+  CKEDITOR.config.autoGrow_onStartup = true;
+  CKEDITOR.config.autoGrow_maxHeight = 800;
+  CKEDITOR.config.autoGrow_minHeight = 300;
+  CKEDITOR.config.autoGrow_bottomSpace = 20;
+  // For allowing classes on divs - http://docs.ckeditor.com/#!/api/CKEDITOR.config
+  CKEDITOR.config.allowedContent = true;
+  // Nice source highlighting
+  CKEDITOR.config.codemirror = {
+    highlightActiveLine: false,
+    theme: 'rubyblue'
+  };
+  // Your custom stylesheet
+  // CKEDITOR.config.contentsCss = '/stylesheets/bootstrap.css';
+  CKEDITOR.config.toolbar =
+  [
+    ['Format'],
     ['Bold','Italic','Strike','-','Subscript','Superscript'],
     ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
-    ['RadiantPreview', 'Paste', 'RemoveFormat'],
-    ['Find','Replace'],
-    ['Image','Paperclipped', 'MediaEmbed', 'Table','HorizontalRule','SpecialChar'],
+    ['Undo','Redo','-','Find','Replace','-', 'Paste', 'RemoveFormat', 'Scayt'],
     ['Link','Unlink','Anchor'],
-    ['Source', '-', 'Maximize']
-	//// 	Alternative toolbar config
-	//    ['Source','-','Templates'],
-	//    ['Cut','Copy','Paste','PasteText','PasteFromWord','-','Print', 'SpellChecker','-','RadiantPreview'],
-	//    ['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
-	//    ['Image','Paperclipped', 'MediaEmbed', 'Table','HorizontalRule','SpecialChar','PageBreak'],
-	//		'/',
-	//    ['Styles','Format'],
-	//    ['Bold','Italic','Underline','Strike','-','Subscript','Superscript'],
-	//    ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
-	//    ['JustifyLeft','JustifyCenter','JustifyRight'],
-	//    ['Link','Unlink','Anchor'],
-	//    ['Maximize','-','About']
-	]
-	CKEDITOR.on('instanceReady',
-		function( evt ) {
-			var editor = evt.editor
-			var ck_holder = $("cke_" + editor.name)
-			if(ck_holder){
-				Droppables.add(ck_holder, {
-					accept: 'asset',
-					onDrop: function(element) {
-						var link = element.select('a.bucket_link')[0]
-						var classes = element.className.split(' ')
-						var tag_type = classes[0]
-						if(tag_type == 'image') {
-			        var tag = '<img src="'+ link.href +'" />'
-			      }
-			      else {
-			        var asset_id = element.id.split('_').last();
-			        var tag = '<a href="'+ link.href +'">'+ link.title +'</a>'
-			      }
-						var element = CKEDITOR.dom.element.createFromHtml(tag)
-						editor.insertElement(element)
-				  }
-				})
-			}
+    ['Image','Paperclipped','MediaEmbed','Table','HorizontalRule','SpecialChar','-','ShowBlocks'],
+    ['RadiantPreview', '-' ,'Source']
+  ],
+  CKEDITOR.on('instanceReady',
+    function( evt ) {
+      var editor = evt.editor;
+      var ck_holder = $("cke_" + editor.name);
+      if(ck_holder){
+        Droppables.add(ck_holder, {
+          accept: 'asset',
+          onDrop: function(element) {
+            var link = element.select('a.bucket_link')[0];
+            var classes = element.className.split(' ');
+            var tag_type = classes[0];
+            if(tag_type == 'image') {
+              var tag = '<img src="'+ link.href +'" />';
+            }
+            else {
+              var asset_id = element.id.split('_').last();
+              var tag = '<a href="'+ link.href +'">'+ link.title +'</a>';
+            }
+            var element = CKEDITOR.dom.element.createFromHtml(tag);
+            editor.insertElement(element);
+          }
+        });
+      }
     }
-	)
-	
-	var usedFilter = $('part_' + partIndex +'_filter_id')
-	if(usedFilter.value == 'CKEditor'){
-		putInEditor(partIndex)
-	}
-	
-	var timer = setInterval(function() { 
-		// Make image asset draggable
-		Asset.MakeDraggables
-		// Make asset bucket thumbnails draggable 
-	  $$('div.resized').each(function(element){
-			if(!element.hasClassName("move"))
-	    	new Draggable(element, { revert: true })
-	    	element.addClassName('move')
-	  })
-	}, 5000);
-	
+  );
+
+  var usedFilter = $('part_' + partIndex +'_filter_id');
+  if(usedFilter.value == 'CKEditor'){
+    putInEditor(partIndex);
+  }
+
+  var timer = setInterval(function() {
+    // Make image asset draggable
+    Asset.MakeDraggables();
+    // Make asset bucket thumbnails draggable
+    $$('div.resized').each(function(element){
+      if(!element.hasClassName("move"));
+        new Draggable(element, { revert: true });
+        element.addClassName('move');
+    });
+  }, 5000);
+
 }
 
 function toggleEditor(partIndex){
-	var filterId = $('part_' + partIndex + '_filter_id')
-	if(filterId.value == 'CKEditor'){
-		putInEditor(partIndex)
-	} else {
-		removeEditor(partIndex)
-	}
+  var filterId = $('part_' + partIndex + '_filter_id');
+  if(filterId.value == 'CKEditor'){
+    putInEditor(partIndex);
+  } else {
+    removeEditor(partIndex);
+  }
 }
 
 function removeEditor(partIndex){
-	var instance = CKEDITOR.instances['part_'+ partIndex +'_content']
-	instance.destroy()
+  var instance = CKEDITOR.instances['part_'+ partIndex +'_content'];
+  instance.destroy();
 }
 
 function putInEditor(partIndex){
-	var textarea = $('part_' + partIndex + '_content')
-	CKEDITOR.replace(textarea)
+  var textarea = $('part_' + partIndex + '_content');
+  CKEDITOR.replace(textarea);
 }
 
 InsertIntoCk = Behavior.create({
@@ -99,17 +112,18 @@ InsertIntoCk = Behavior.create({
     var textbox = $('part_' + part_name + '_content');
 
     var tag_parts = this.element.getAttribute('rel').split('_');
-    var href = this.element.getAttribute('href')
+    var href = this.element.getAttribute('href');
     var tag_name = tag_parts[0];
     var asset_size = tag_parts[1];
     var asset_id = tag_parts[2];
 
     if($('part_' + part_name + '_filter_id').value == 'CKEditor'){
-      editor = CKEDITOR.instances['part_'+ part_name +'_content']
-      if(tag_name == 'image')
-        editor.insertHtml("<img src=\"" + href + "\" alt=\"\" />")
-      else
-        editor.insertHtml("<a href=\"" + href + "\">" + this.element.up(".back").down(".title").innerHTML + "</a>")
+      editor = CKEDITOR.instances['part_'+ part_name +'_content'];
+      if(tag_name == 'image') {
+        editor.insertHtml("<img src=\"" + href + "\" alt=\"\" />");
+      } else {
+        editor.insertHtml("<a href=\"" + href + "\">" + this.element.up(".back").down(".title").innerHTML + "</a>");
+      }
     }
   }
 });
